5060fd3847fc74729fa12d2f2a4c5e5fd9d06e1d
Arthur <arthur.boyer@gmail.com>
2020-05-09 09:14:54 +1200

    Refactoring complete on simulation-3, but results wrong

--- scratch.R
+++ scratch.R
@@ -1,17 +1,42 @@
 # Clear the environment
 # rm(list = ls())
 
-num_sims=3
-sim_size=100
-p_placebo=0.5
+num_trials=3
+num_subjects=100
+p_control=0.5
 p_treatment=0.8
 
-matrices = replicate(num_sims, 
-run_sim(sim_size=sim_size, 
- p_placebo=p_placebo, 
- p_treatment=p_treatment), 
-simplify = FALSE)
 
+do_trial <- function(p_control=0.5, p_treatment=0.5, num_subjects=100){
+    binom_control = rbinom(1,num_subjects,p_control)
+    binom_treatment = rbinom(1,num_subjects,p_treatment)
+    rbind(control=c(live=binom_control, 
+                    die=(num_subjects - binom_control)),
+        treatment=c(live=binom_treatment, 
+                    die=(num_subjects - binom_treatment)))
+}
+
+
+# do_trial_2 <- function(p_control=0.5, p_treatment=0.5, num_subjects=100){
+#     binom_control = rbinom(1,num_subjects,p_control)
+#     binom_treatment = rbinom(1,num_subjects,p_treatment)
+#     rbind(live=c(control=binom_control, 
+#                  treatment=binom_treatment),
+#           die=c(control=(num_subjects - binom_control), 
+#                 treatment=(num_subjects - binom_treatment)))
+# }
+
+matrices = replicate(num_trials, 
+                     do_trial(p_control=p_control, 
+                             p_treatment=p_treatment,
+                             num_subjects=num_subjects), 
+                     simplify = FALSE)
+
+# matrices = replicate(num_trials, 
+#                      do_trial_2(p_control=p_control, 
+#                              p_treatment=p_treatment,
+#                              num_subjects=num_subjects), 
+#                      simplify = FALSE)
 
 data.frame(simulation=matrices)
 
--- simulation-3.R
+++ simulation-3.R
@@ -1,82 +1,89 @@
-num_iterations = 1000
-placebo_prob = 0.5
-treatment_prob = 0.6
-test_size = 100
+rm(list = ls()) # clear all vars from the current workspace
 
-#install.packages("functional")
-#library(functional)
-
-run_sim <- function(p_placebo=0.5, p_treatment=0.5, sim_size=100){
-    test_1 <- rbinom(1,sim_size,p_placebo)
-    test_1_minus100 <- sim_size - test_1
-    dead_alive_1 <- c(test_1, test_1_minus100)
-    test_2 <- rbinom(1,sim_size,p_treatment)
-    test_2_minus100 <- sim_size - test_2
-    dead_alive_2 <- c(test_2, test_2_minus100)
-    matrix_dead_alive <- rbind(dead_alive_1,dead_alive_2)
-    return(matrix_dead_alive)
+do_trial <- function(p_control=0.5, p_treatment=0.8, num_subjects=100){
+    binom_control = rbinom(1,num_subjects,p_control)
+    binom_treatment = rbinom(1,num_subjects,p_treatment)
+    rbind(control=c(live=binom_control, 
+                    die=(num_subjects - binom_control)),
+        treatment=c(live=binom_treatment, 
+                    die=(num_subjects - binom_treatment)))
 }
 
-to_chisq <- function(m){
-    return(chisq.test(m, correct = TRUE))
+chisq_parts <- function(trial){
+    chi <- chisq.test(trial, correct = TRUE)
+    squared <- chi$statistic[["X-squared"]]
+    p <- chi$p.value
+    result <- c(p_value=p, x_squared=squared)
+    return(result)
 }
 
-is_significant <- function(chi){
-    return(chi$p.value >= 0.03 && chi$p.value <= 0.05)
+is_significant <- function(p_value){
+    return(p_value >= 0.03 && p_value <= 0.05)
 }
 
-is_significant_matrix <- function(m){
-    return(is_significant(to_chisq(m)))
+calculate_phi <- function(x_squared, num_subjects){
+    sqrt((x_squared * x_squared)/num_subjects)
 }
 
-to_phi <- function(chi){
-    chi$statistic
+analyze_trials <- function(trial_list){
+    chi = Map(chisq_parts, trial_list)
+    parts = c('p_value', 'x_squared')
+    df <- data.frame(do.call(rbind, lapply(chi, `[`, parts)))
+
+    calculate_phi <- function(x_squared){
+        sqrt((x_squared * x_squared)/num_subjects)
+    }
+
+    df$phi <- do.call(calculate_phi, df['x_squared'])
+    df$significant <- do.call(is_significant, df['p_value'])
+    return(df)
 }
 
-generate_table <- function(num_sims=3, 
-                           sim_size=100, 
-                           p_placebo=0.5, 
+display_comparison <- function(control_statistics, treatment_statistics){
+    control_significant = subset(control_statistics, significant==TRUE)
+    treatment_significant = subset(treatment_statistics, significant==TRUE)
+    num_false_positives = nrow(control_significant)
+    num_true_positives = nrow(num_true_positives)
+    false_positive_risk = num_false_positives / (num_true_positives + num_false_positives)
+    
+    cat( "\n",
+        "False Positives: ", num_false_positives, "\n",
+        "True Positives: ", num_true_positives, "\n",
+        "False Positive Risk: ", false_positive_risk, "\n")
+    #TODO: graph the differences
+}
+
+run_trials <- function(num_trials,
+                       num_subjects,
+                       p_control, 
+                       p_treatment){
+    replicate(num_trials, 
+              do_trial(p_control=p_control, 
+                       p_treatment=p_treatment,
+                       num_subjects=num_subjects), 
+              simplify = FALSE)
+}
+
+run_simulation <- function(num_trials=3,
+                           num_subjects=100,
+                           p_control=0.5,
                            p_treatment=0.8){
-    matrices = replicate(num_sims, 
-                         run_sim(sim_size=sim_size, 
-                                 p_placebo=p_placebo, 
-                                 p_treatment=p_treatment), 
-                         simplify = FALSE)
-    sims <- cbind(simulation=matrices)
-    sims <- cbind(sims, chisq=Map(to_chisq, sims[,"simulation"]))
-    sims <- cbind(sims, phi=Map(to_phi, sims[,"chisq"]))
-    sims <- cbind(sims, significant=Map(is_significant, sims[,"chisq"]))
-    return(sims)
+    control_trials = run_trials(num_trials, num_subjects, p_control, p_control)
+    treatment_trials = run_trials(num_trials, num_subjects, p_control, p_treatment)
+
+    control_statistics = analyze_trials(control_trials)
+    treatment_statistics = analyze_trials(treatment_trials)
+
+    display_comparison(control_statistics, treatment_statistics)
 }
 
-placebo_vs_placebo = generate_table(p_treatment=placebo_prob)
-treatment_vs_placebo = generate_table(p_treatment=treatment_prob)
+num_trials=10000
+num_subjects=100
+p_control=0.5
+p_treatment=0.8
+
+run_simulation(num_trials,num_subjects,p_control,p_treatment)
 
 
-
-# sims[,"simulation"]
-
-
-# placebo_vs_placebo <- replicate(num_iterations, binom_matrix(placebo_prob,placebo_prob), simplify = FALSE)
-# treatment_vs_placebo <- replicate(num_iterations, binom_matrix(placebo_prob,treatment_prob), simplify = FALSE)
-# 
-# placebo_vs_placebo_chi <- Map(to_chisq, placebo_vs_placebo)
-# treatment_vs_placebo_chi <- Map(to_chisq, treatment_vs_placebo)
-# 
-# false_positives = Filter(is_significant, placebo_vs_placebo_chi)
-# true_positives = Filter(is_significant, treatment_vs_placebo_chi)
-# 
-# num_false_positives = length(false_positives)
-# num_true_positives = length(true_positives)
-# false_positive_risk = num_false_positives / (num_true_positives + num_false_positives)
-# 
-# cat("False Positives: ", num_false_positives, "\n",
-#     "True Positives: ", num_true_positives, "\n",
-#     "False Positive Risk: ", false_positive_risk)
-# 
-# 
-# 
-# 
-# as.vector(replicate(3, binom_matrix(placebo_prob,placebo_prob), simplify = FALSE))
 
 

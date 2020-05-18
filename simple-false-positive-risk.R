e36779f3ad78d69c197609941c06e3318a699895
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2020-05-19 11:16:26 +1200

    wording to type 1 2 error + effect size

--- simple-false-positive-risk.R
+++ simple-false-positive-risk.R
@@ -6,6 +6,7 @@ setwd("~/GitHub/false-positive-rates")
 if(!exists("monteCarloFisher", mode="function")) 
     sourceCpp("monte-carlo-fisher.cpp")
 
+a <- c(0.1)
 simTrials <- function (
     monteCarloSims = 10000L,
     participantsPerArm = 100L,
@@ -13,41 +14,59 @@ simTrials <- function (
     absoluteRR = 0.1) {
     
     ctrlOutcomes <- rbinom(monteCarloSims, participantsPerArm, baselineRisk)
+    rxRisk <- baselineRisk - absoluteRR
     
     df <- monteCarloFisher(n = participantsPerArm * 2L,
                            outcomes = cbind(
                                rbinom(monteCarloSims, participantsPerArm, baselineRisk), # no dif interv
                                ctrlOutcomes, # control group
-                               rbinom(monteCarloSims, participantsPerArm, baselineRisk - absoluteRR), # working Rx
+                               rbinom(monteCarloSims, participantsPerArm, rxRisk), # working Rx
                                ctrlOutcomes))
                                             
     
-    sub <- "no difference"
+    sub <- paste0("2 arms both risk:", baselineRisk)
     hist(df$p.1, sub=sub)
     hist(df$p.1[df$p.1 < 0.1], sub=sub)
     
-    sub <- paste0("control:", baselineRisk, "; intervention:", baselineRisk - absoluteRR)
+    sub <- paste0("control:", baselineRisk, "; intervention:", rxRisk)
     hist(df$p.2, sub=sub)
     hist(df$p.2[df$p.2 < 0.1], sub=sub)
     
-    false_rx_works <- sum(df$or.1 < 1 & df$p.1 < 0.05)
-    false_rx_harmfull <- sum(df$or.1 > 1 & df$p.1 < 0.05)
-    highly_false_pos <- sum(df$p.1 < 0.02)
+    type1_tail1 <- sum(df$or.1 < 1.0 & df$p.1 < 0.05)
+    type1_tail2 <- sum(df$or.1 > 1.0 & df$p.1 < 0.05)
     
-    false_negative <- sum(df$or.2 > 1 | df$p.2 >= 0.05)
-    false_rx_type3 <- sum(df$or.2 > 1 & df$p.2 < 0.05)
+    type2 <- sum(df$p.2 >= 0.05)
+    type3 <- sum(df$or.2 > 1.0 & df$p.2 < 0.05)
+    
+    a <<- df
     
     resultOut <- function(text, count){
         return(paste0("\n\t", text, ": ", count, " (", 100 * count / monteCarloSims, "%)"))
     }
-    cat("NO DIFFERENCE:", 
-        resultOut("treatment shown to work", false_rx_works),
-        resultOut("treatment shown to be harmful", false_rx_harmfull),
-        resultOut("false positive rate (p < 0.05)", false_rx_works + false_rx_harmfull),
-        resultOut("includes highly false positive (p < 0.02)", highly_false_pos),
-        "\nDIFFERENCE (", sub, "):",
-        resultOut("false negative rate ", false_negative),
-        resultOut("including opposite finding of harm (p < 0.05)", false_rx_type3))
+    cat("Simulated trials:", prettyNum(monteCarloSims, big.mark = " "),
+        "\nAllocations per Trial Arm:", participantsPerArm,
+        "\n-------------------------",
+        "\nEqual Risks (", baselineRisk, "):", 
+        resultOut("type 1 errors made: tail 1", type1_tail1),
+        resultOut("type 1 errors made: tail 2", type1_tail2),
+        resultOut("total type 1 errors made (FP)", type1_tail1 + type1_tail1),
+        resultOut("includes type 1 with p < 0.02", sum(df$p.1 < 0.02)),
+        "\nRisk Reduction (", sub, "):",
+        resultOut("type 2 errors made", type2),
+        resultOut("type 3 errors made", type3),
+        resultOut("type 2 or 3 errors", type2 + type3),
+        resultOut("no error (TP)", monteCarloSims - type2 - type3),"\n")
+    
+    cat("\n\n\nPoint effect estimate when type 1 error encountered (Odds Ratio) truth = 1.0:\n")
+    print(summary(df$or.1[which(df$p.1 < 0.05 & df$or.1 > 1.0)]))
+    print(summary(df$or.1[which(df$p.1 < 0.05 & df$or.1 < 1.0)]))
+    cat("\nPoint effect estimate when no error (Odds Ratio) truth = ",
+        rxRisk * (1 - baselineRisk) / (baselineRisk * (1 - rxRisk)), 
+        ":\n")
+    print(summary(df$or.2[which(df$p.2 < 0.05 & df$or.2 < 1.0)]))
+    cat("confidence intervals of the odds ratios (lower then upper bounds)\n")
+    print(summary(df$ci_lb.2[which(df$p.2 < 0.05 & df$or.2 < 1.0)]))
+    print(summary(df$ci_ub.2[which(df$p.2 < 0.05 & df$or.2 < 1.0)]))
 }
 
 simTrials(

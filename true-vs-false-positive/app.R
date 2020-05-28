ff7753a4aa78e0afed8cebec5fc6fdc6a6cce920
Brent McSharry <2456704+mcshaz@users.noreply.github.com>
2020-05-29 10:54:59 +1200

    add 2 plots to monte-carlo-results

--- true-vs-false-positive/app.R
+++ true-vs-false-positive/app.R
@@ -9,6 +9,11 @@
 
 library(shiny)
 library(pwr)
+library(ggplot2)
+
+toLookupPercent <- function(v) {
+    as.integer(100 * (v + .Machine$double.eps))
+}
 
 data <- readRDS("millionRuns.RDS")
 data <- data[order(data$trialN),]
@@ -17,15 +22,15 @@ spData <- split(data, data$riskCtrl==data$riskRx)
 ctrlCtrl <- subset(spData$`TRUE`, select=c("trialN", "riskCtrl","nonSig"))
 convertDta <- 1e-4 # div 1 million but as percentage
 ctrlCtrl$falsePos <- 100 - ctrlCtrl$nonSig * convertDta
-ctrlCtrl$riskCtrl <- 100 * ctrlCtrl$riskCtrl
+ctrlCtrl$riskCtrl <- toLookupPercent(ctrlCtrl$riskCtrl)
 ctrlCtrl$nonSig <- NULL
 
 
 ctrlRx <- subset(spData$`FALSE`, select=-c(nonSig))
 ctrlRx$RxHarm <- ctrlRx$RxHarm * convertDta
 ctrlRx$RxBenefit <- ctrlRx$RxBenefit * convertDta
-ctrlRx$riskCtrl <- 100 * ctrlRx$riskCtrl
-ctrlRx$riskRx <- 100 * ctrlRx$riskRx
+ctrlRx$riskCtrl <- toLookupPercent(ctrlRx$riskCtrl)
+ctrlRx$riskRx <- toLookupPercent(ctrlRx$riskRx)
 
 data <- NULL
 spData <- NULL
@@ -64,7 +69,8 @@ ui <- fluidPage(
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput(outputId = "againstN", width="100%")
+           plotOutput(outputId = "againstN", width="100%"),
+           plotOutput(outputId = "againstXRisk", width="100%")
         )
     )
 )
@@ -88,49 +94,34 @@ server <- function(input, output){
                 i <- i + 1
             }
         }
-        return(returnVar)
+        reshapeNm <- colnames(returnVar)[-1]
+
+        return(reshape(data=returnVar, idvar="trialN",
+                                 varying = reshapeNm,
+                                 v.names=c("value"),
+                                 times=reshapeNm,
+                                 direction="long"))
     })
     
-    reactive_XN <- reactive({
-        ctrlNames <- intersect(c("trialN", input$show), names(ctrlCtrl))
-        intervNames <- intersect(input$show, names(ctrlRx))
-        returnVar <- data.frame(
-            ctrlCtrl[input$nRng[1] <= ctrlCtrl$trialN & ctrlCtrl$trialN <= input$nRng[2]
-                     & ctrlCtrl$riskCtrl == input$riskRng[2], ctrlNames],
-            ctrlRx[input$nRng[1] <= ctrlRx$trialN & ctrlRx$trialN <= input$nRng[2] 
-                   & ctrlRx$riskCtrl == input$riskRng[2] & ctrlRx$riskRx == input$riskRng[1], intervNames])
-        colnames(returnVar) <- c(ctrlNames, intervNames)
-        if ("pwr" %in% input$show) {
-            h <- 2 * asin(sqrt(input$riskRng[1] / 100)) - 2 * asin(sqrt(input$riskRng[2] / 100))
-            i <- 1
-            returnVar$pwr <- rep(0,nrow(returnVar))
-            for (n in returnVar[,1]) {
-                returnVar$pwr[i] <- unname(pwr.p.test(h=h,n=n,sig.level=0.05,alternative="two.sided")[["power"]]) * 100
-                i <- i + 1
-            }
-        }
-        return(returnVar)
+    reactive_XRisk <- reactive({
+        return(ctrlRx[input$nRng[1] <= ctrlRx$trialN & ctrlRx$trialN <= input$nRng[2] 
+                            & input$riskRng[1] <= ctrlRx$riskRx & ctrlRx$riskRx < input$riskRng[2]
+                            & ctrlRx$riskCtrl == input$riskRng[2],c("riskRx", "RxBenefit", "trialN")])
     })
-
     
     output$againstN <- renderPlot({
-        colours <- c(RxBenefit = "blue", falsePos = "red", RxHarm = "green", pwr = "purple")
-        nDta <- reactive_XN()
-        nms <- names(nDta)
-        nc <- ncol(nDta)
-        
-        plot(nDta[,1], nDta[,2], type = "b", pch = 19, ylab = "percent", col=colours[nms[2]],
-             xlab = "Number of participnants(N)",
-             sub=paste("baseline risk", input$riskRng[2], "vs intervention risk", input$riskRng[1]),
-             ylim=range( c(min(nDta[-1]), max(nDta[-1])) ))
-        if (nc > 2) {
-            for (i in 3:nc) {
-                lines(nDta[,1], nDta[,i], type = "b", lty = 1, lwd=1, col=colours[nms[i]])
-            }
-        }
-        
-        legend("topleft", nms[-1],
-               col=colours[intersect(nms, names(colours))], lty = 1:2, cex = 0.8)
+        ggplot(data = reactive_XN(), aes(x=trialN, y=value, group=time)) +
+            geom_line(aes(color=time))+
+            geom_point(aes(color=time))+
+            labs(y="Percentage (%)", x = "Trial Participants (N)", colour = "Variable:",
+                 caption=paste("Baseline (control) risk ",input$riskRng[2], "% vs ",input$riskRng[1],"%"))
+    })
+    output$againstXRisk <- renderPlot({
+        ggplot(data = reactive_XRisk(), aes(x=riskRx, y=RxBenefit, group=trialN)) +
+            geom_line(aes(color=trialN))+
+            geom_point(aes(color=trialN))+
+            labs(y="Percentage (%)", x = "Intervention Group Risk", colour = "N:",
+                 caption=paste("Baseline (control) risk ",input$riskRng[2], "%"))
     })
 }
 shinyApp(ui=ui, server=server)
